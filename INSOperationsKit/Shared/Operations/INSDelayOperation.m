2419dbb95ad2262e4e31da2d6e8e597fb32ea006
Timur Kuchkarov <timurbk91@gmail.com>
2016-03-12 22:36:34 +0600

    Correct delay interval usage.

--- INSOperationsKit/Shared/Operations/INSDelayOperation.m
+++ INSOperationsKit/Shared/Operations/INSDelayOperation.m
@@ -49,7 +49,7 @@
     }
 
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(self.delay * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(interval * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         if (![self isCancelled]) {
             [self finish];
         }
